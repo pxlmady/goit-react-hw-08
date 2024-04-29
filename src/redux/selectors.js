5ee1dfb3a5c6a50d70d5012baf93d2a57b715a10
pxlmady <yoplufre@gmail.com>
2024-04-29 20:01:11 +0300

    fristinit

--- src/redux/selectors.js
+++
@@ -1,4 +0,0 @@
-export const selectIsLoading = (state) => state.contacts.isLoading;
-export const selectError = (state) => state.contacts.error;
-export const selectContacts = (state) => state.contacts.items;
-export const selectNameFilter = (state) => state.filters.name;
